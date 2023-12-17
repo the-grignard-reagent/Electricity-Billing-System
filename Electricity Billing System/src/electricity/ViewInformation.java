8afac4d543ad310e139eb3d1f3aadaab5685504b
the-grignard-reagent <76035351+the-grignard-reagent@users.noreply.github.com>
2023-12-17 19:04:43 +0530

    Change#017: Updated ViewInformation page

--- Electricity Billing System/src/electricity/ViewInformation.java
+++ Electricity Billing System/src/electricity/ViewInformation.java
@@ -3,80 +3,85 @@ import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
 
-public class ViewInformation extends JFrame implements ActionListener{
+public class ViewInformation extends JFrame implements ActionListener {
 
     JButton cancel;
+
+    // Constructor for ViewInformation class
     ViewInformation(String meter) {
+        // Setting up the frame
         setBounds(350, 150, 850, 650);
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        
-        
+
+        // Adding heading JLabel
         JLabel heading = new JLabel("VIEW CUSTOMER INFORMATION");
         heading.setBounds(250, 0, 500, 40);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(heading);
-        
+
+        // Labels for displaying customer information
         JLabel lblname = new JLabel("Name");
         lblname.setBounds(70, 80, 100, 20);
         add(lblname);
-        
+
         JLabel name = new JLabel("");
         name.setBounds(250, 80, 100, 20);
         add(name);
-        
+
         JLabel lblmeternumber = new JLabel("Meter Number");
         lblmeternumber.setBounds(70, 140, 100, 20);
         add(lblmeternumber);
-        
+
         JLabel meternumber = new JLabel("");
         meternumber.setBounds(250, 140, 100, 20);
         add(meternumber);
-        
+
         JLabel lbladdress = new JLabel("Address");
         lbladdress.setBounds(70, 200, 100, 20);
         add(lbladdress);
-        
+
         JLabel address = new JLabel("");
         address.setBounds(250, 200, 100, 20);
         add(address);
-        
+
         JLabel lblcity = new JLabel("City");
         lblcity.setBounds(70, 260, 100, 20);
         add(lblcity);
-        
+
         JLabel city = new JLabel("");
         city.setBounds(250, 260, 100, 20);
         add(city);
-        
+
         JLabel lblstate = new JLabel("State");
         lblstate.setBounds(500, 80, 100, 20);
         add(lblstate);
-        
+
         JLabel state = new JLabel("");
         state.setBounds(650, 80, 100, 20);
         add(state);
-        
+
         JLabel lblemail = new JLabel("Email");
         lblemail.setBounds(500, 140, 100, 20);
         add(lblemail);
-        
+
         JLabel email = new JLabel("");
         email.setBounds(650, 140, 100, 20);
         add(email);
-        
+
         JLabel lblphone = new JLabel("Phone");
         lblphone.setBounds(500, 200, 100, 20);
         add(lblphone);
-        
+
         JLabel phone = new JLabel("");
         phone.setBounds(650, 200, 100, 20);
         add(phone);
-        
+
+        // Fetching customer information from the database
         try {
             Conn c = new Conn();
-            ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '"+meter+"'");
-            while(rs.next()) {
+            ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '" + meter + "'");
+            while (rs.next()) {
                 name.setText(rs.getString("name"));
                 address.setText(rs.getString("address"));
                 city.setText(rs.getString("city"));
@@ -88,28 +93,32 @@ public class ViewInformation extends JFrame implements ActionListener{
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
+        // Button for canceling the operation
         cancel = new JButton("Cancel");
         cancel.setBackground(Color.BLACK);
         cancel.setForeground(Color.WHITE);
         cancel.setBounds(350, 340, 100, 25);
         add(cancel);
         cancel.addActionListener(this);
-        
+
+        // Adding an image to the frame
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/viewcustomer.jpg"));
         Image i2 = i1.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(20, 350, 600, 300);
         add(image);
-        
+
         setVisible(true);
     }
-    
+
+    // ActionListener for handling button clicks
     public void actionPerformed(ActionEvent ae) {
         setVisible(false);
     }
-    
+
+    // Main method to create an instance of ViewInformation class
     public static void main(String[] args) {
         new ViewInformation("");
     }
