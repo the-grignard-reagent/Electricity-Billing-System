4395604ad5285a91a9dce60915f743de0a4a5dac
the-grignard-reagent <76035351+the-grignard-reagent@users.noreply.github.com>
2023-12-17 18:27:36 +0530

    Change#002: Updated Conn

--- Electricity Billing System/src/electricity/Conn.java
+++ Electricity Billing System/src/electricity/Conn.java
@@ -1,20 +1,29 @@
 import java.sql.*;
 
-
+// Conn class for database connection
 public class Conn {
-    
+
+    // Connection and Statement objects for database interaction
     Connection c;
-    Statement  s;
+    Statement s;
+
+    // Constructor to initialize the database connection
     Conn() {
-        //JDBC
+        // JDBC driver loading is not required in recent JDBC versions
         // Class.forName("/com.mysql.cj.jdbc.Driver");
+
         try {
+            // Establish a connection to the MySQL database
+            // Replace "localhost" with the actual database server address
+            // "3306" is the default port for MySQL, and "ebs" is the database name
+            // Replace "RMgX" and "example-password" with the actual username and password
             c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "RMgX", "example-password");
+
+            // Create a Statement object for executing SQL queries
             s = c.createStatement();
-        }catch(Exception e){
+        } catch (Exception e) {
+            // Print the stack trace if there's an exception during the database connection setup
             e.printStackTrace();
         }
-        
     }
-    
 }
