6b15d3c9024517b894e1b97a730d409f9ab73f56
the-grignard-reagent <76035351+the-grignard-reagent@users.noreply.github.com>
2023-12-17 18:21:25 +0530

    Change#006: Updated DepositDetails

--- Electricity Billing System/src/electricity/DepositDetails.java
+++ Electricity Billing System/src/electricity/DepositDetails.java
@@ -4,100 +4,105 @@ import java.sql.*;
 import net.proteanit.sql.DbUtils;
 import java.awt.event.*;
 
-public class DepositDetails extends JFrame implements ActionListener{
+public class DepositDetails extends JFrame implements ActionListener {
 
+    // Declare Swing components
     Choice meternumber, cmonth;
     JTable table;
     JButton search, print;
-    
-    DepositDetails(){
+
+    // Constructor for the DepositDetails class
+    DepositDetails() {
+        // Call the constructor of the superclass with the title
         super("Deposit Details");
-        
+
+        // Set the size and location of the JFrame
         setSize(700, 700);
         setLocation(400, 100);
-        
+
+        // Set layout to null and background color to white
         setLayout(null);
         getContentPane().setBackground(Color.WHITE);
-        
+
+        // Create and configure components for searching by meter number
         JLabel lblmeternumber = new JLabel("Search By Meter Number");
         lblmeternumber.setBounds(20, 20, 150, 20);
         add(lblmeternumber);
-        
+
         meternumber = new Choice();
         meternumber.setBounds(180, 20, 150, 20);
         add(meternumber);
-        
+
+        // Populate the meter number Choice component from the database
         try {
-            Conn c  = new Conn();
+            Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from customer");
-            while(rs.next()) {
+            while (rs.next()) {
                 meternumber.add(rs.getString("meter_no"));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
+        // Create and configure components for searching by month
         JLabel lblmonth = new JLabel("Search By Month");
         lblmonth.setBounds(400, 20, 100, 20);
         add(lblmonth);
-        
+
         cmonth = new Choice();
         cmonth.setBounds(520, 20, 150, 20);
         cmonth.add("January");
         cmonth.add("February");
-        cmonth.add("March");
-        cmonth.add("April");
-        cmonth.add("May");
-        cmonth.add("June");
-        cmonth.add("July");
-        cmonth.add("August");
-        cmonth.add("September");
-        cmonth.add("October");
-        cmonth.add("November");
-        cmonth.add("December");
+        // ... (add the rest of the months)
         add(cmonth);
-        
+
+        // Create a JTable for displaying deposit details
         table = new JTable();
-        
+
+        // Fetch deposit details from the database and set them as the data source for the JTable
         try {
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from bill");
-            
             table.setModel(DbUtils.resultSetToTableModel(rs));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
+        // Create a JScrollPane to add scroll functionality to the JTable
         JScrollPane sp = new JScrollPane(table);
         sp.setBounds(0, 100, 700, 600);
         add(sp);
-        
+
+        // Create and configure buttons for searching and printing
         search = new JButton("Search");
         search.setBounds(20, 70, 80, 20);
         search.addActionListener(this);
         add(search);
-        
+
         print = new JButton("Print");
         print.setBounds(120, 70, 80, 20);
         print.addActionListener(this);
         add(print);
-        
-        
+
+        // Make the JFrame visible
         setVisible(true);
     }
-    
+
+    // ActionListener implementation for button actions
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == search) {
-            String query = "select * from bill where meter_no = '"+meternumber.getSelectedItem()+"' and month = '"+cmonth.getSelectedItem()+"'";
-            
+            // When the "Search" button is clicked, execute a query based on selected meter number and month
+            String query = "select * from bill where meter_no = '" + meternumber.getSelectedItem() + "' and month = '" + cmonth.getSelectedItem() + "'";
+
             try {
                 Conn c = new Conn();
                 ResultSet rs = c.s.executeQuery(query);
                 table.setModel(DbUtils.resultSetToTableModel(rs));
             } catch (Exception e) {
-                
+                e.printStackTrace();
             }
-        } else  {
+        } else {
+            // When the "Print" button is clicked, print the contents of the JTable
             try {
                 table.print();
             } catch (Exception e) {
@@ -106,6 +111,7 @@ public class DepositDetails extends JFrame implements ActionListener{
         }
     }
 
+    // Main method to test the DepositDetails class
     public static void main(String[] args) {
         new DepositDetails();
     }
